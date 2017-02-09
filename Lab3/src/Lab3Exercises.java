81a8743ae094db2328fe03080d80b15001afe987
Rafael <Rafael@DESKTOP-KSCENEV>
2017-02-08 21:42:24 -0400

    Diagonal lines and oval drawings.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Rafael");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(530, 250);
+		myFrame.setSize(400, 400);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
+		
 	}
 }
